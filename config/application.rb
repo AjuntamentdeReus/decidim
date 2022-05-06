5e88720e394f8f48dac1ad293ff34fc4f92141db
Fernando Blat <fernando@blat.es>
2022-05-06 05:02:31 +0200

    Fix locales

--- config/application.rb
+++ config/application.rb
@@ -12,10 +12,11 @@ module DecidimApplication
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "*.{yml}")]
+    config.i18n.available_locales = %w(ca es en)
+    config.i18n.default_locale = :en
+    config.i18n.enforce_available_locales = false
+    config.i18n.fallbacks = {ca: [:en], es: [:en]}
+
   end
 end
 
--- config/initializers/01_locales.rb
+++
@@ -1,5 +0,0 @@
-I18n.default_locale = :en
-I18n.available_locales = [:ca, :es]
-I18n.enforce_available_locales = false
-I18n.fallbacks = {ca: [:en], es: [:en]}
-
