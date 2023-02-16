86af7781ef415a6bdeb944808f870f60807a04bb
Fernando Blat <fernando@blat.es>
2023-02-16 11:02:50 +0100

    Fix regex

--- app/packs/src/decidim/decidim_application.js
+++ app/packs/src/decidim/decidim_application.js
@@ -1,3 +1,3 @@
 $(document ).ready(function() {
-  $('#new_authorization_handler #authorization_handler_document_number').attr('pattern', '^([a-zA-Z][0-9]{7})|([0-9]{8})$');
+  $('#new_authorization_handler #authorization_handler_document_number').attr('pattern', '^[0-9]{8}$|^[a-zA-Z][0-9]{7}$');
 });
