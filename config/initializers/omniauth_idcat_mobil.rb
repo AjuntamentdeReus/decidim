56c5c6c2ad02f7f7245ffd1b4645184fce27697a
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2024-05-24 16:47:18 +0200

    Replace idcat_mobil with trusted_ids adapted module and install

--- config/initializers/omniauth_idcat_mobil.rb
+++ config/initializers/omniauth_idcat_mobil.rb
@@ -1,7 +1,12 @@
-Devise.setup do |config|
-  config.omniauth :idcat_mobil,
-                  ENV["IDCAT_MOBIL_CLIENT_ID"],
-                  ENV["IDCAT_MOBIL_CLIENT_SECRET"],
-                  ENV["IDCAT_MOBIL_SITE_URL"],
-                  scope: :autenticacio_usuari
+Decidim::TrustedIds.configure do |config|
+  # The name of the omniauth provider, must be registered in Decidim.
+  config.omniauth_provider = "valid"
+  config.omniauth = {
+    enabled: true,
+    client_id: ENV["IDCAT_MOBIL_CLIENT_ID"],
+    client_secret: ENV["IDCAT_MOBIL_CLIENT_SECRET"],
+    site: ENV["IDCAT_MOBIL_SITE_URL"],
+    scope: "autenticacio_usuari",
+    icon: "smartphone-line"
+  }
 end
